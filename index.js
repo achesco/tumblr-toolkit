e0d4e35475e32cb066989a82654097059d53ead1
Anton Lysenko <achesco@gmail.com>
2017-07-11 22:38:41 +0300

    testable arch

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 #! /usr/bin/env node
 
 const argv = require('./lib/cli').cli.argv;
-require(`./tools/${argv._[0]}`)(argv);
+const tool = require(`./tools/${argv._[0]}`)(argv);
+const feed = require('./lib/feed')(tool.cli);
+feed.stream(tool.processor);
