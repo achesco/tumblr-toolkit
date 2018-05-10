ffbf6c539e29d60d54abb0837c5582b24815d62a
Anton Lysenko <achesco@gmail.com>
2018-05-10 11:41:56 +0300

    process exceptions/errors handling added

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 #! /usr/bin/env node
 
+process.on('unhandledRejection', reason => {
+	console.error('Unhandled rejection', reason);
+});
+
+process.on('uncaughtException', error => {
+	console.error('Uncaught exception', error);
+	process.exit(1);
+});
+
 const argv = require('./lib/cli').cli.argv;
 const tool = require(`./tools/${argv._[0]}`)(argv);
 const feed = require('./lib/feed')(tool.cli);
